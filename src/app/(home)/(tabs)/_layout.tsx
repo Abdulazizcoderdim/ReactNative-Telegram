eb595a4a1599224c73a86fcea634ab0c39718623
AbdulazizRus <coderdim@gmail.com>
2024-05-26 19:43:26 +0500

    changes

--- src/app/(home)/(tabs)/_layout.tsx
+++ src/app/(home)/(tabs)/_layout.tsx
@@ -25,4 +25,3 @@ export default function TabsNavigator() {
     </Tabs>
   );
 }
-// 30:00
--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,19 +1,19 @@
-import {useState} from "react"
-import { Text } from "react-native"
-import { Channel, ChannelList, MessageList } from "stream-chat-expo"
+import { useState } from 'react'
+import { Text } from 'react-native'
+import { Channel, ChannelList, MessageInput, MessageList } from 'stream-chat-expo'
 
 export default function MainTabScreen() {
-    const [channel, setChannel] = useState();
-    console.log(channel)
+  const [channel, setChannel] = useState()
+  console.log(channel)
 
-    if (channel) {
-        return <Channel channel={channel}>
-            <MessageList/>
-        </Channel>
-    }   
-
-
-    return(
-        <ChannelList onSelect={(channel)}/>
+  if (channel) {
+    return (
+      <Channel channel={channel}>
+        <MessageList />
+        <MessageInput />
+      </Channel>
     )
+  }
+
+  return <ChannelList onSelect={channel} />
 }
